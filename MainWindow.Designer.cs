5df658e2dbe5e375f9255e67c8f91412966c3a13
Lê Khai Trí <lekhaitri.2003@gmail.com>
2022-12-19 14:02:12 +0700

    Add project files.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Scheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.period_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timeUnit_box = new System.Windows.Forms.ComboBox();
+            this.shutdown_tm = new System.Windows.Forms.Timer(this.components);
+            this.remaining_tm = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Chọn thao tác:\r\n";
+            // 
+            // period_box
+            // 
+            this.period_box.Location = new System.Drawing.Point(160, 104);
+            this.period_box.Name = "period_box";
+            this.period_box.Size = new System.Drawing.Size(107, 22);
+            this.period_box.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Thời gian hẹn giờ:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Bắt đầu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.start_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 21);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Standby";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(160, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 28);
+            this.panel1.TabIndex = 15;
+            // 
+            // timeUnit_box
+            // 
+            this.timeUnit_box.FormattingEnabled = true;
+            this.timeUnit_box.Items.AddRange(new object[] {
+            "second",
+            "minute",
+            "hour"});
+            this.timeUnit_box.Location = new System.Drawing.Point(273, 102);
+            this.timeUnit_box.Name = "timeUnit_box";
+            this.timeUnit_box.Size = new System.Drawing.Size(75, 24);
+            this.timeUnit_box.TabIndex = 17;
+            // 
+            // shutdown_tm
+            // 
+            this.shutdown_tm.Interval = 10000;
+            // 
+            // remaining_tm
+            // 
+            this.remaining_tm.Interval = 60000;
+            this.remaining_tm.Tick += new System.EventHandler(this.remaining_tm_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 195);
+            this.Controls.Add(this.timeUnit_box);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.period_box);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scheduler";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox period_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox timeUnit_box;
+        private System.Windows.Forms.Timer shutdown_tm;
+        private System.Windows.Forms.Timer remaining_tm;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Tulpep.NotificationWindow;
+using Timer;
+
+namespace Scheduler
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timeUnit_box.Text = "minute";
+            period_box.Text = "25";
+        }
+
+        private void timer_alert(object sender, EventArgs e)
+        {
+            shutdown_tm.Stop();
+            shutdown_tm.Tick -= timer_alert;
+
+            PopupNotifier popup = new PopupNotifier();
+            popup.Image = Timer.Properties.Resources.clock;
+            popup.TitleText = "Hurry!";
+            popup.ContentText = "Dập máy sau 5 phút 0 giây";
+            popup.Popup();
+
+            shutdown_tm.Interval = 300000;
+            shutdown_tm.Tick += timer_shutdown;
+            shutdown_tm.Start();
+        }
+
+        private void timer_shutdown(object sender, EventArgs e)
+        {
+            shutdown_tm.Stop();
+            shutdown_tm.Tick -= timer_shutdown;
+            label1.Text = "Thời gian hẹn giờ:";
+            period_box.Text = "25";
+
+            string filename = string.Empty;
+            string arguments = string.Empty;
+
+            //if (radioButton1.Checked == true)
+            //{
+            //    filename = "shutdown.exe";
+            //    arguments = "-s";
+            //}
+
+            //ProcessStartInfo startExecute = new ProcessStartInfo(filename, arguments);
+            //Process.Start(startExecute);
+            //MessageBox.Show("Sleep");
+
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+
+        }
+
+        int tick_time = 1000;
+        private void start_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int period = Convert.ToInt32(this.period_box.Text);
+                switch (timeUnit_box.Text)
+                {
+                    case "second":
+                        {
+                            tick_time = 1000;
+                            period *= 1000;
+                            
+                            break;
+                        }
+
+                    case "minute":
+                        {
+                            tick_time = 60 * 1000;
+                            period *= 60 * 1000;
+
+                            break;
+                        }
+
+                    case "hour":
+                        {
+                            //period_box.Text = $"{--period}";
+                            tick_time = 60 * 60 * 100;
+                            period *= 60 * 60 * 1000;
+
+                            break;
+                        }
+                }
+
+                if (period <= 300000)
+                {
+                    PopupNotifier popup = new PopupNotifier();
+                    popup.Image = Timer.Properties.Resources.clock;
+                    popup.TitleText = "Hurry!";
+                    popup.ContentText = $"Dập máy sau {period / 60000} phút {period / 1000} giây";
+                    popup.Popup();
+
+                    shutdown_tm.Interval = period;
+                    shutdown_tm.Tick += timer_shutdown;
+                    shutdown_tm.Start();
+                }
+                else
+                {
+                    shutdown_tm.Interval = period - 300000;
+                    shutdown_tm.Tick += timer_alert;
+                    shutdown_tm.Start();
+                }
+
+                AnnounceWindow announceWindow = new AnnounceWindow();
+                announceWindow.ShowDialog();
+                this.WindowState = FormWindowState.Minimized;
+
+                remaining_tm.Interval = tick_time;
+                remaining_tm.Start();
+                label1.Text = "Thời gian còn lại:";
+            }
+            catch
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin");
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to Exit?",
+                        "Hey!",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void remaining_tm_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
